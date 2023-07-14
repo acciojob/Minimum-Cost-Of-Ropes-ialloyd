c4c4afe2b4ad12a41f7a5fbb4b3604016e27d23e
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-14 18:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function calculateMinCost() {
+	
   //your code here
+
+	let str = document.forms["form"]["rope-lengths"].value;
+
+	let arr=str.split(',')
+
+	let new_arr=[]
+
+	for(let x of arr){
+
+		new_arr.push(Number(x))
+		
+	}
+
+	while(true){
+
+
+		
+	}
+	
+	
+
+	
+	
+	
+	document.getElementById('result').innerHTML =typeof new_arr[0]
   
   
   
